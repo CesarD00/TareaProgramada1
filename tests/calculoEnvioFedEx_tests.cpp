854aa5c0c56edc34effe64a245bfaa76362e1df2
CesarD00 <cesar.diazmora@ucr.ac.cr>
2022-04-22 11:57:53 -0600

    Modificación escenarios de pruebas unitarias.

--- tests/calculoEnvioFedEx_tests.cpp
+++ tests/calculoEnvioFedEx_tests.cpp
@@ -2,7 +2,7 @@
 #include "../src/envioFedEx.h"
 
 namespace {
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMayor500_PesoMayor10) {
+    TEST(EnvioFedEx_CalculoEnvio_Test, Test_DistanciaMayor500_PesoMayor10) {
         /// AAA
 
         // Arange - se configura el escenario
@@ -18,7 +18,7 @@ namespace {
         EXPECT_DOUBLE_EQ(esperada, actual);
     }   
 
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMayor500_PesoMenor10) {
+    TEST(EnvioFedEx_CalculoEnvio_Test, Test_DistanciaMayor500_PesoMenorOIgual10) {
         /// AAA
 
         // Arange - se configura el escenario
@@ -34,7 +34,7 @@ namespace {
         EXPECT_DOUBLE_EQ(esperada, actual);
     }  
 
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMenor500_PesoMayor10) {
+    TEST(EnvioFedEx_CalculoEnvio_Test, Test_DistanciaMenorOIgual500_PesoMayor10) {
         /// AAA
 
         // Arange - se configura el escenario
@@ -50,23 +50,7 @@ namespace {
         EXPECT_DOUBLE_EQ(esperada, actual);
     }  
 
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMenor500_PesoMenor10) {
-        /// AAA
-
-        // Arange - se configura el escenario
-        EnvioFedEx* envio = new EnvioFedEx(9, 499);
-
-        // Act - se ejecuta la operación
-        double actual = envio->calculoEnvio();
-        double esperada = 35;
-
-        delete envio;
-
-        // Assert - se validan los resultados
-        EXPECT_DOUBLE_EQ(esperada, actual);
-    }  
-
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaIgual500_PesoIgual10) {
+    TEST(EnvioFedEx_CalculoEnvio_Test, Test_DistanciaMenorOIgual500_PesoMenorOIgual10) {
         /// AAA
 
         // Arange - se configura el escenario
@@ -82,68 +66,6 @@ namespace {
         EXPECT_DOUBLE_EQ(esperada, actual);
     }  
 
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaIgual500_PesoMenor10) {
-        /// AAA
 
-        // Arange - se configura el escenario
-        EnvioFedEx* envio = new EnvioFedEx(9, 500);
-
-        // Act - se ejecuta la operación
-        double actual = envio->calculoEnvio();
-        double esperada = 35;
-
-        delete envio;
-
-        // Assert - se validan los resultados
-        EXPECT_DOUBLE_EQ(esperada, actual);
-    }  
-
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaIgual500_PesoMayor10) {
-        /// AAA
-
-        // Arange - se configura el escenario
-        EnvioFedEx* envio = new EnvioFedEx(11, 500);
-
-        // Act - se ejecuta la operación
-        double actual = envio->calculoEnvio();
-        double esperada = 45;
-
-        delete envio;
-
-        // Assert - se validan los resultados
-        EXPECT_DOUBLE_EQ(esperada, actual);
-    } 
-
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMenor500_PesoIgual10) {
-        /// AAA
-
-        // Arange - se configura el escenario
-        EnvioFedEx* envio = new EnvioFedEx(10, 499);
-
-        // Act - se ejecuta la operación
-        double actual = envio->calculoEnvio();
-        double esperada = 35;
-
-        delete envio;
-
-        // Assert - se validan los resultados
-        EXPECT_DOUBLE_EQ(esperada, actual);
-    }  
-
-    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_DistanciaMayor500_PesoIgual10) {
-        /// AAA
-
-        // Arange - se configura el escenario
-        EnvioFedEx* envio = new EnvioFedEx(10, 501);
-
-        // Act - se ejecuta la operación
-        double actual = envio->calculoEnvio();
-        double esperada = 50;
-
-        delete envio;
-
-        // Assert - se validan los resultados
-        EXPECT_DOUBLE_EQ(esperada, actual);
-    }  
 
 }
---
+++ tests/calculoEnvioServicioPostal_tests.cpp
@@ -0,0 +1,118 @@
+#include <gtest/gtest.h>
+#include "../src/envioServicioPostal.h"
+
+namespace {
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_Peso0a3_Clase1) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(2, 5000, 1);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 1500;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_Peso0a3_Clase2) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(2, 5000, 2);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 140;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_Peso4a8_Clase1) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(6, 5000, 1);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 2250;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_Peso4a8_Clase2) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(6, 5000, 2);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 265;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_PesoIgualOMayor9_Clase1) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(10, 5000, 1);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 3000;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }   
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_PesoIgualOMayor9_Clase2) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(10, 5000, 2);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 375;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }   
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_PesoCualquiera_Clase3) {
+        /// AAA
+
+        // Arange - se configura el escenario
+        EnvioServicioPostal* envio = new EnvioServicioPostal(2, 5000, 3);
+
+        // Act - se ejecuta la operación
+        double actual = envio->calculoEnvio();
+        double esperada = 60;
+
+        delete envio;
+
+        // Assert - se validan los resultados
+        EXPECT_DOUBLE_EQ(esperada, actual);
+    }   
+
+    
+}
